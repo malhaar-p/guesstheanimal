0b60555f887fc8d6236e9acf214dedb6da2b7f4f
malhaar-p <malhaar@flipgroup.com.au>
2017-11-26 00:09:07 +1100

    first commit

---
+++ src/GuessTheAnimal/Models/Animal.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GuessTheAnimal.Models
+{
+    public class Animal
+    {
+        public string Name { get; set; }
+        public List<string> Hints { get; set; }
+    }
+}
---
+++ src/GuessTheAnimal/ViewModels/AnimalsViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+using GuessTheAnimal.Models;
+using System.IO;
+using Newtonsoft;
+using Newtonsoft.Json.Linq;
+
+namespace GuessTheAnimal.ViewModels
+{
+    public class AnimalsViewModel : ObservableCollection<Animal>
+    {
+        public AnimalsViewModel()
+        {
+            // Read the json file
+            // Add the animals into collection.
+
+
+
+        }
+
+        public static IList<Animal> GetAnimalsFromJson(string filePath)
+        {
+            JObject o = JObject.Parse(File.ReadAllText(filePath));
+            JArray a = (JArray) o["animals"];
+
+            IList<Animal> animals = a.ToObject<IList<Animal>>();
+
+            return animals;
+        }
+    }
+}
